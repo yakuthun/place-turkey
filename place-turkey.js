6d5b64923af15c8c0e78935de5ffd986b8af63e5
yakuthun <31483097+yakuthun@users.noreply.github.com>
2022-04-03 09:48:55 +0300

    Update place-turkey.js

--- place-turkey.js
+++ place-turkey.js
@@ -7,23 +7,22 @@
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
-
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-       
-        const image = document.createElement("https://raw.githubusercontent.com/yakuthun/place-turkey/master/flag.png");
-        image.src = "";
+        // Load the image
+        const image = document.createElement("img");
+        image.src = "https://raw.githubusercontent.com/yakuthun/place-turkey/master/flag.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
       
-      
+        // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const canvas = camera.querySelector("mona-lisa-canvas");
         canvas.shadowRoot.querySelector('.container').appendChild(image);
       
-       
+        // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
             const preview = camera.querySelector("mona-lisa-pixel-preview");
             if (preview) {
