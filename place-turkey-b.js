c0adfa9ac0121887e714c5cf49a7fb06bb8cc9cd
yakuthun <31483097+yakuthun@users.noreply.github.com>
2022-04-03 09:55:51 +0300

    black-flag-update

--- place-turkey-b.js
+++ place-turkey-b.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://github.com/yakuthun/place-turkey/blob/master/black-flag.png";
+        image.src = "https://raw.githubusercontent.com/yakuthun/place-turkey/master/black-flag.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
