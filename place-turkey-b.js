29776223a5f046a1d1ace8dbeebd8a59fe96e614
yakuthun <31483097+yakuthun@users.noreply.github.com>
2022-04-03 09:53:01 +0300

    black-added

--- place-turkey-b.js
+++ place-turkey-b.js
@@ -1,30 +1,28 @@
+// ==UserScript==
 // @name         Turkey! Logo template
 // @namespace    http://tampermonkey.net/
 // @version      0.3
-// @description  try to take over the canvas!
+// @description   Düzenleme isteyenler DC: Cavalera#8739 - yakuthun - http://github.com/yakuthun
 // @author       oralekin, LittleEndu, ekgame
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
-// @contact      Düzenleme isteyenler DC: Cavalera#8739
-//arrangement    yakuthun
-//@github        http://github.com/yakuthun
-
+// ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-       
-        const image = document.createElement("/flag.png");
-        image.src = "";
+        // Load the image
+        const image = document.createElement("img");
+        image.src = "https://github.com/yakuthun/place-turkey/blob/master/black-flag.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
       
-      
+        // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const canvas = camera.querySelector("mona-lisa-canvas");
         canvas.shadowRoot.querySelector('.container').appendChild(image);
       
-       
+        // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
             const preview = camera.querySelector("mona-lisa-pixel-preview");
             if (preview) {
